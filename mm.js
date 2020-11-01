5f64523fc85298472705cc4c0ae1cc10f6e81c7a
robinnsinghhh <72340031+robinnsinghhh@users.noreply.github.com>
2020-11-01 14:42:38 +0530

    My Website

---
+++ mm.js
@@ -0,0 +1,10 @@
+<script>
+var d = new Date();
+d.setFullYear(2020);
+document.getElementById("demo").innerHTML = d;
+
+var d = new Date();
+d.setFullYear(2020, 11, 3);
+document.getElementById("demo").innerHTML = d;
+</script>
+
